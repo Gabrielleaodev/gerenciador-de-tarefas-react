e5e768bffb6502602e70065d2bd790fd67e2b890
Gabriel Leão <gabrielleao335@gmail.com>
2026-06-08 18:28:33 -0400

    commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 import { useState } from 'react';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import {v4} from 'uuid';
+import { v4 } from 'uuid';
+import { useEffect } from 'react';
 
 
-function App(){
-  const [tasks,setTasks] = useState ([
-    {
-      id: v4(),
-      title: 'Estudar React',
-      description: 'Aprender os conceitos básicos do React e criar um projeto simples.',
-      isCompleted: false
-    },
-    {
-      id: v4(),
-      title: 'Fazer Exercícios',
-      description: 'Resolver exercícios de lógica de programação para praticar.',
-      isCompleted: false
-    },
-    {
-      id: v4(),
-      title: 'Ler Documentação',
-      description: 'Ler a documentação oficial do React para entender melhor as funcionalidades.',
-      isCompleted: false
-    }
-  ]);
+function App() {
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
-//FUNÇÃO PARA ATUALIZAR O ESTADO DAS TAREFAS, QUANDO CLICAR NA TAREFA, ELA VAI RECEBER O ID DA TAREFA CLICADA
+  useEffect(() => {
+localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //FUNÇÃO PARA ATUALIZAR O ESTADO DAS TAREFAS, QUANDO CLICAR NA TAREFA, ELA VAI RECEBER O ID DA TAREFA CLICADA
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       //PRECISO ATUALIZAR ESSA TAREFA, SE O ID FOR IGUAL AO ID DA TAREFA CLICADA
@@ -42,30 +28,30 @@ function App(){
   }
 
   function onDeliteTaskClick(taskId) {
-const newTasks = tasks.filter((task) => task.id !== taskId);
-setTasks(newTasks);
-  }    
-
-function onAddTaskSubmit(title, description) {
-  const newTask = {
-    id: v4(),
-    title,
-    description,
-    isCompleted: false
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
   }
-  setTasks([...tasks, newTask]);
-}
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false
+    }
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
-     <div className='w-[500px] space-y-4'>
-       <h1 className='text-3xl text-slate-100 font-bold text-center'>
-        Gerenciador de Tarefas</h1>
-         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
-      <Tasks tasks={tasks} 
-      onTaskClick={onTaskClick} 
-      onDeliteTaskClick={onDeliteTaskClick}/>
-     </div>
+      <div className='w-[500px] space-y-4'>
+        <h1 className='text-3xl text-slate-100 font-bold text-center'>
+          Gerenciador de Tarefas</h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeliteTaskClick={onDeliteTaskClick} />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,38 +1,40 @@
 import { ChevronRightIcon, DeleteIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 function Tasks(props) {
   const navigate = useNavigate();
 
-  function onSeeDetailsClick(task ) {
+  function onSeeDetailsClick(task) {
     const query = new URLSearchParams()
     query.set("title", task.title);
     query.set("description", task.description);
     navigate(`task?${query.toString()}`);
   }
-   return (
-     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-       {props.tasks.map((task) =>
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) =>
 
-         <li key={task.id}className="flex gap-2">
-            <button onClick={() => props.onTaskClick(task.id)} 
+        <li key={task.id} className="flex gap-2">
+          <button onClick={() => props.onTaskClick(task.id)}
             className={`bg-slate-400 p-2 text-left w-full rounded-md text-white ${task.isCompleted ? 'line-through text-black' : ''}`}>
-                {task.title}
-                 </button>
+            {task.title}
+          </button>
 
-            <button onClick={()=> onSeeDetailsClick(task)} className="bg-slate-400 p-2 rounded-md text-white">
-               <ChevronRightIcon />
-                </button>
-            <button onClick={() => props.onDeliteTaskClick(task.id)}  className="bg-slate-400 p-2 rounded-md text-white">
-                <DeleteIcon />
-                </button>
-                 
-            </li>
+          <Button onClick={() => onSeeDetailsClick(task)} >
+            <ChevronRightIcon />
+          </Button>
+
+          <Button onClick={() => props.onDeliteTaskClick(task.id)}>
+            <DeleteIcon />
+          </Button>
+
+        </li>
 
 
-       )}
-     </ul>
-   );
+      )}
+    </ul>
+  );
 }
 
 export default Tasks;
